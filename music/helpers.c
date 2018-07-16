e3f5d4b5a40c7d471af267688df5230039dec845
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-16 00:00:31 +0000

    added parameters to the duration

--- music/helpers.c
+++ music/helpers.c
@@ -1,20 +1,26 @@
 // Helper functions for music
 
 #include <cs50.h>
-
+#include <stdlib.h>
+#include <string.h>
 #include "helpers.h"
 
 // Converts a fraction formatted as X/Y to eighths
-int duration(string fraction)
-{
-    // TODO
-}
+  int duration(string fraction)
+    {
+      int X = atoi (&fraction[0]);
+      int Y = atoi (&fraction[2]);
+
+      int eighths = (8/Y)*X;
+
+      return eighths;
+    }
 
 // Calculates frequency (in Hz) of a note
-int frequency(string note)
-{
+   // int frequency(string note)
+   // {
     // TODO
-}
+   // }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
