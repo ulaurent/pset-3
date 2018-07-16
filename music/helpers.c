14211183f2a5a0a913d1f1b62661d4e6917b967b
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-16 22:45:05 +0000

    prelim solution to frequency function

--- music/helpers.c
+++ music/helpers.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include "helpers.h"
 
-// Converts a fraction formatted as X/Y to eighths
+// Converts a fraction formatted as /Y to eighths
   int duration(string fraction)
     {
       int X = atoi (&fraction[0]);
@@ -17,10 +17,35 @@
     }
 
 // Calculates frequency (in Hz) of a note
-   // int frequency(string note)
-   // {
+    int frequency(string note)
+    {
+        int hertz;
+        int octave;
+        int place;
+        string letter;
+        //string accidental;
+
+        //First step find out what Octave you are in
+        if(strlen(note) == 2){
+            if(note[0]=='A' && note[1]== 4){
+                hertz = 440;
+                return hertz;
+            }
+            else{
+                letter = &note[0];
+                octave = note[1];
+                place = 440 *((octave - 4) * 2);
+            }
+        }
+    }
+        /*else{
+            letter = note[0];
+            accidental = note[1];
+            octave = note[2];
+
+        }*/
     // TODO
-   // }
+
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
