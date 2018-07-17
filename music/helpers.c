88db475cac1093a4b32296bda7373ab8dfd10c3f
Urbenson Laurent <ulaurent12@gmail.com>
2018-07-17 21:46:46 +0000

    prelim sol. needs editing

--- music/helpers.c
+++ music/helpers.c
@@ -8,8 +8,8 @@
 // Converts a fraction formatted as /Y to eighths
   int duration(string fraction)
     {
-      int X = atoi (&fraction[0]);
-      int Y = atoi (&fraction[2]);
+      int X = (int) fraction[0] - '0';
+      int Y = (int) fraction[2] - '0';
 
       int eighths = (8/Y)*X;
 
@@ -22,29 +22,132 @@
         int hertz;
         int octave;
         int place;
-        string letter;
-        //string accidental;
+        int section;
+        char letter;
+        char accidental;
 
-        //First step find out what Octave you are in
-        if(strlen(note) == 2){
-            if(note[0]=='A' && note[1]== 4){
+            //First step find out what Octave you are in
+            // If its A4 auto return hertz equal to 440
+            if(note[0]=='A' && note[1]== '4'){
                 hertz = 440;
                 return hertz;
             }
+            
             else{
-                letter = &note[0];
-                octave = note[1];
-                place = 440 *((octave - 4) * 2);
-            }
-        }
-    }
-        /*else{
-            letter = note[0];
-            accidental = note[1];
-            octave = note[2];
+                if(strlen(note) == 2){
+                    letter = note[0];
+                    octave = (int)note[1];
+                    place = 440 *((octave - 4) * 2);
 
-        }*/
-    // TODO
+                    if (note[0] == 'B'){
+                        hertz = place*(2^(2/12));
+                    }
+                    if (note[0] == 'G'){
+                        hertz = place/(2^(2/12));
+                    }
+                    if (note[0] == 'F'){
+                        hertz = place/(2^(4/12));
+                    }
+                    if (note[0] == 'E'){
+                        hertz = place/(2^(5/12));
+                    }
+                    if (note[0] == 'D'){
+                        hertz = place/(2^(7/12));
+                    }
+                    if (note[0] == 'C'){
+                        hertz = place/(2^(9/12));
+                    }
+                }
+                //Parameters if string consist of accidentals
+                else {
+                    letter = note[0];
+                    accidental = note[1];
+                    octave = (int)note[2];
+                    place = 440 *((octave - 4) * 2);
+
+                    if (letter == 'B'){
+                        section = place*(2^(2/12));
+
+                        if(accidental == '#'){
+                            hertz = section * (2^(2/12));
+                            return hertz;
+                        }
+                        else{
+                        // accidental is a flat 'b'
+                            hertz = section /(2^(1/12));
+                            return hertz;
+                        }
+                    }
+
+                    if (note[0] == 'G'){
+                        section = place/(2^(2/12));
+
+                        if(accidental == '#'){
+                            hertz = section * (2^(2/12));
+                            return hertz;
+                        }
+                        else{
+                        // accidental is a flat 'b'
+                            hertz = section /(2^(1/12));
+                            return hertz;
+                        }
+                    }
+                    if (note[0] == 'F'){
+                        section = place/(2^(4/12));
+
+                        if(accidental == '#'){
+                            hertz = section * (2^(2/12));
+                            return hertz;
+                        }
+                        else{
+                        // accidental is a flat 'b'
+                            hertz = section /(2^(1/12));
+                            return hertz;
+                        }
+                    }
+                    if (note[0] == 'E'){
+                        section = place/(2^(5/12));
+                        if(accidental == '#'){
+                            hertz = section * (2^(2/12));
+                            return hertz;
+                        }
+                        else{
+                        // accidental is a flat 'b'
+                            hertz = section /(2^(1/12));
+                            return hertz;
+                        }
+                    }
+                    if (note[0] == 'D'){
+                        section = place/(2^(7/12));
+                        if(accidental == '#'){
+                            hertz = section * (2^(2/12));
+                            return hertz;
+                        }
+                        else{
+                        // accidental is a flat 'b'
+                            hertz = section /(2^(1/12));
+                            return hertz;
+                        }
+                    }
+                    if (note[0] == 'C'){
+                        section = place/(2^(9/12));
+
+                        if(accidental == '#'){
+                            hertz = section * (2^(2/12));
+                            return hertz;
+                        }
+                        else{
+                        // accidental is a flat 'b'
+                            hertz = section /(2^(1/12));
+                            return hertz;
+                        }
+                    }
+
+
+                }
+            }
+    }
+    
 
 
 // Determines whether a string represents a rest
